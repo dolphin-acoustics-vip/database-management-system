ebd649e9607eb0d6943454218c699c4e430c9818
js521 <js521@LAPTOP-AIB48LK5>
2024-03-28 20:54:28 +0000

    Fix automatic file to set OS variables
    Add a newline character to each line written to /.bashrc to ensure
    each command is being treated as a separate line. Before, the lines
    would concatenate to each other resulting in corrupted values.

--- set_os_variables.py
+++ set_os_variables.py
@@ -5,7 +5,7 @@ with open(os.path.expanduser("~/.bashrc"), "a") as outfile:
     # note the <user_name> is usually root
     # note the <user_password> is updated in the MariaDB shell with
     #       mysql> ALTER USER 'user_name'@'host' IDENTIFIED BY 'user_password';
-    outfile.write("export STADOLPHINACOUSTICS_HOST=<host>")
-    outfile.write("export STADOLPHINACOUSTICS_USER=<user_name>")
-    outfile.write("export STADOLPHINACOUSTICS_PASSWORD=<user_password>")
-    outfile.write("export STADOLPHINACOUSTICS_DATABASE=<database_name>")
+    outfile.write("export STADOLPHINACOUSTICS_HOST=<host>\n")
+    outfile.write("export STADOLPHINACOUSTICS_USER=<user_name>\n")
+    outfile.write("export STADOLPHINACOUSTICS_PASSWORD=<user_password>\n")
+    outfile.write("export STADOLPHINACOUSTICS_DATABASE=<database_name>\n")
