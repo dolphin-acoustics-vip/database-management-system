c0b6be48efbcabd5a062ee7cee8256f1a29e5762
JamieSullivan12 <sullivanj041@gmail.com>
2024-03-31 22:06:15 +0100

    Clean up routes_selection
    Add comments, docstrings and in general clean up the routes in
    routes_selection.py.

--- routes/routes_selection.py
+++ routes/routes_selection.py
@@ -1,34 +1,40 @@
-import re, uuid, zipfile, os
+# Standard library imports
+import re
+
+# Third-party imports
 from flask import Blueprint, flash,get_flashed_messages, jsonify, redirect,render_template,request, send_file,session, url_for, send_from_directory
 from sqlalchemy.orm import joinedload,sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
-from db import FILE_SPACE_PATH, Session, GOOGLE_API_KEY
-import db
-from models import *
 
+# Location application imports
+from db import FILE_SPACE_PATH, Session, GOOGLE_API_KEY, db, parse_alchemy_error
+from models import *
 
 routes_selection = Blueprint('selection', __name__)
 
-
 def insert_or_update_selection(session, selection_number, file, recording_id, selection_id=None):
+    """
+    A function to either insert a new Selection object or update an existing one in the database.
+    
+    Parameters
+    - session: the database session
+    - selection_number: the number of the selection
+    - file: the file to be associated with the selection
+    - recording_id: the id of the recording
+    - selection_id: the id of the selection (optional, default is None)
+    :return: the Selection object that was inserted or updated
+    """
     recording = session.query(Recording).filter_by(id=recording_id).first()
-
-
+    # get Selection object if updating or make a new one if inserting (i.e. selection_id is None)
     if selection_id is not None:
         selection_obj = session.query(Selection).filter_by(id=selection_id).first()
     else:
         selection_obj = Selection()
     selection_obj.recording = recording
     session.add(selection_obj)
-    
     selection_obj.set_selection_number(selection_number)
-
-    # Handle recording file
-
     selection_file = file
-
     selection_filename = selection_obj.generate_filename()
-
     selection_relative_path = selection_obj.generate_relative_path()
     new_file = File()
     try:
@@ -42,28 +48,37 @@ def insert_or_update_selection(session, selection_number, file, recording_id, se
         raise e
     new_file.set_uploaded_date(datetime.now())
     new_file.set_uploaded_by("User 1")
-
     selection_obj.selection_file = new_file
     selection_obj.selection_file_id = new_file.id
-
     session.add(new_file)
-
     return selection_obj
 
-
 @routes_selection.route('/process_selection', methods=['GET'])
-def process_selection_number():
+def process_selection():
+    """
+    Process the selection of a large number of selection files by the user.
+    
+    HTTP request arguments:
+    - recording_id (str): The ID of the recording to which the selection file will belong.
+    - filename (str): The filename of the selection file being processed.
+    - selection_number (str): The number of the selection being processed.
+    
+    Returns:
+        A JSON response containing the selection number, a list of messages, and a validity flag.
+        - selection_number (str): The selected number.
+        - messages (list): A list of messages indicating any warnings or errors encountered during processing.
+        - valid (bool): A flag indicating if the selection is valid.
+    """
     recording_id = request.args.get('recording_id')
     filename = request.args.get('filename')
     selection_number = request.args.get('selection_number')
-    valid = True
-    messages=[]
-
-    if selection_number == None or str(selection_number).strip() == "":
-        selection_number = None
-
-
+    valid = True # flag
+    messages=[] # to return at the end
     
+    # Prevent empty strings from causing issues in the future
+    if str(selection_number).strip() == "":
+        selection_number = None
+        
     # Extract the selection number from the filename using regular expression
     match = re.search(r'sel_(\d+)', filename)
     if match:
@@ -81,19 +96,16 @@ def process_selection_number():
     else:
         messages.append("Selection number: " + selection_number + ".")
     
-
-
-
+    # Check if selection number already exists
     with Session() as session:
-
         if selection_number != None:
             selection_number_exits = session.query(Selection).filter(db.text("selection_number = :selection_number and recording_id = :recording_id")).params(selection_number=selection_number, recording_id=recording_id).first()
             if selection_number_exits:
                 messages.append("<span style='color: red;'>Error: selection number already exists.</span>")
                 valid=False
-
-        recording = session.query(Recording).filter(db.text("id = :recording_id")).params(recording_id=recording_id).first()
         
+        # Check if the selection start time matches that of its recording
+        recording = session.query(Recording).filter(db.text("id = :recording_id")).params(recording_id=recording_id).first()
         match = re.search(r'(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})', filename)
         if match:
             year = match.group(1)
@@ -102,15 +114,14 @@ def process_selection_number():
             hour = match.group(4)
             minute = match.group(5)
             second = match.group(6)
-            
             date_string = f"{day}/{month}/{year} {hour}:{minute}:{second}"
             date = datetime.strptime(date_string, '%d/%m/%Y %H:%M:%S')
             if not recording.match_start_time(date):
                 messages.append("Warning: start time mismatch.")
-
         else:
             messages.append("Warning: no start time.")
     
+    # Check if selection number is an integer
     if selection_number != None and selection_number != "":
         try:
             int(selection_number)
@@ -120,11 +131,18 @@ def process_selection_number():
 
     return jsonify(selection_number=selection_number,messages=messages,valid=valid)
 
-
 @routes_selection.route('/encounter<uuid:encounter_id>/recording/<uuid:recording_id>/selection/insert-bulk', methods=['GET', 'POST'])
 def selection_insert_bulk(encounter_id,recording_id):
-    try:
-        with Session() as session:
+    """
+    Inserts multiple selection files into the database for a given encounter ID and recording ID.
+    
+    Note that the metadata for these selection files should first be checked using the /process_selection GET route.
+    
+    Returns:
+        A JSON response indicating success or failure.
+    """
+    with Session() as session:
+        try:
             if 'selection_files' not in request.files:
                 return jsonify({'error': 'No files found'}), 400
 
@@ -132,28 +150,24 @@ def selection_insert_bulk(encounter_id,recording_id):
             files = request.files.getlist('selection_files')
             selection_numbers = [request.form.get(f'selection_numbers[{i}]') for i in range(len(files ))]
 
-            print(files)
-            print(selection_numbers)
-            # Process the files
+            # Process the files and add them to the Selection object
             for i, file in enumerate(files):
                 try:
-
-
                     insert_or_update_selection(session,selection_numbers[i], file, recording_id)
                     session.commit()
                     flash(f'Added selection {selection_numbers[i]}', 'success')
                 except SQLAlchemyError as e:
                     session.rollback()
-                    flash(db.parse_alchemy_error(e), 'error')
+                    flash(parse_alchemy_error(e), 'error')
                 except IOError as e:
                     session.rollback()
                     flash(str(e), 'error')
             session.commit()
             return jsonify({'message': 'Files uploaded successfully'}), 200
-    except SQLAlchemyError as e:
-        flash(db.parse_alchemy_error(e), 'error')
-        return jsonify({'error': str(e)}), 500
-    except IOError as e:
-        flash(str(e), 'error')
-        return jsonify({'error': str(e)}), 500
-    
+        except SQLAlchemyError as e:
+            flash(parse_alchemy_error(e), 'error')
+            return jsonify({'error': str(e)}), 500
+        except IOError as e:
+            flash(str(e), 'error')
+            return jsonify({'error': str(e)}), 500
+        
