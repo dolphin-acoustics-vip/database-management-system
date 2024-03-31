d8dd18a0921e3593b5eb0da56266ad7ac013ad0d
JamieSullivan12 <sullivanj041@gmail.com>
2024-03-31 20:36:44 +0100

    Clean routes_admin.py
    Improve the efficiency and readability:
    * Adding consistent commenting and docstrings
    * Ensuring sessions are closed in a resource-efficient manner
    * Make white spacing consistent throughout hte code

--- routes/routes_admin.py
+++ routes/routes_admin.py
@@ -1,13 +1,13 @@
-import re, uuid, zipfile, os
+# Third-party imports
 from flask import Blueprint, flash,get_flashed_messages, jsonify, redirect,render_template,request, send_file,session, url_for, send_from_directory
 from sqlalchemy.orm import joinedload,sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
-from db import Session
-import db
+
+# Local application imports
+from db import Session, parse_alchemy_error
 from models import *
 
 routes_admin = Blueprint('admin', __name__)
-
 
 @routes_admin.route('/admin')
 def admin():
@@ -19,48 +19,36 @@ def admin():
 @routes_admin.route('/admin/dashboard')
 def admin_dashboard():
     """
-    A route decorator for the admin dashboard page. Retrieves data from the database tables DataSource and RecordingPlatform using the session object. Renders the admin dashboard template with the retrieved data lists.
+    A route for the admin dashboard page.
     """
     with Session() as session:
         data_source_list = session.query(DataSource).all()
         recording_platform_list = session.query(RecordingPlatform).all()
         species_list = session.query(Species).all()
-
         return render_template('admin/admin-dashboard.html', data_source_list=data_source_list, recording_platform_list=recording_platform_list, species_list=species_list)
 
 @routes_admin.route('/admin/data-source/<uuid:data_source_id>/view', methods=['GET'])
 def admin_data_source_view(data_source_id):
     """
-    Renders the edit data source page for the given data source ID.
-
-    Parameters:
-        data_source_id (UUID): The ID of the data source.
-
-    Returns:
-        flask.Response: The rendered edit data source page.
+    Route for viewing a specific data source in the admin panel.
     """
     with Session() as session:
         try:
             data_source = session.query(DataSource).filter_by(id=data_source_id).first()  
             return render_template('admin/admin-data-source-view.html', data_source=data_source, data_source_type_values  = DataSource.type.type.enums)
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
             return redirect(url_for('admin.admin_dashboard'))
         
 @routes_admin.route('/admin/data-source/<uuid:data_source_id>/edit', methods=['POST'])
 def admin_data_source_edit(data_source_id):
     """
-    Updates a data source in the database with the provided data. 
-
-    Parameters:
-    data_source_id (uuid): The unique identifier of the data source to be edited.
-
-    Returns:
-    Redirect: Redirects to the administration portal after updating the data source.
+    Update the data for a data source in the admin panel.
     """
     with Session() as session:
         try:
+            # Update data source with form data
             data_source = session.query(DataSource).filter_by(id=data_source_id).first()  
             data_source.name = request.form['name']
             data_source.phone_number1 = request.form['phone_number1']
@@ -70,25 +58,25 @@ def admin_data_source_edit(data_source_id):
             data_source.address = request.form['address']
             data_source.notes = request.form['notes']
             data_source.type = request.form['source-type']
+            # Apply changes
             session.commit()
             flash('Data source updated: {}'.format(data_source.name), 'success')
-            return redirect(url_for('admin.admin_dashboard'))
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
-            return redirect(url_for('admin.admin_dashboard'))
+    return redirect(url_for('admin.admin_dashboard'))
 
 @routes_admin.route('/admin/data-source/new', methods=['GET'])
 def admin_data_source_new():
     """
-    A route decorator that handles GET requests for creating a new data source in the admin panel.
+    A route for the new data source page.
     """
     return render_template('admin/admin-data-source-new.html', data_source_type_values = DataSource.type.type.enums)
 
 @routes_admin.route('/admin/data-source/insert', methods=['POST'])
 def admin_data_source_insert():
     """
-    A function to handle the insertion of a new data source using form data.
+    A route to insert a new data source into the database.
     """
     with Session() as session:
         try:
@@ -103,23 +91,19 @@ def admin_data_source_insert():
                 notes=request.form['notes'],
                 type=request.form['source-type']
             )
+            # Apply changes
             session.add(new_data_source)
             session.commit()
             flash('Data source created: {}'.format(new_data_source.name), 'success')
-            return redirect(url_for('admin.admin_dashboard'))
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
-            return redirect(url_for('admin.admin_dashboard'))
+    return redirect(url_for('admin.admin_dashboard'))
 
 @routes_admin.route('/admin/data-source/<uuid:data_source_id>/delete', methods=['GET'])
 def admin_data_source_delete(data_source_id):
     """
-    A function to delete a data source identified by the given data_source_id.
-    It removes the data source from the database and redirects to the administration portal.
-    
-    Parameters:
-    data_source_id (uuid): The unique identifier of the data source to be deleted.
+    Delete a data source in the admin panel.
     """
     with Session() as session:
         try:
@@ -127,50 +111,42 @@ def admin_data_source_delete(data_source_id):
             session.delete(data_source)
             session.commit()
             flash('Data source deleted: {}'.format(data_source.name), 'success')
-            return redirect(url_for('admin.admin_dashboard'))
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
-            return redirect(url_for('admin.admin_dashboard'))
+    return redirect(url_for('admin.admin_dashboard'))
 
 @routes_admin.route('/admin/recording-platform/<uuid:recording_platform_id>/view', methods=['GET'])
 def admin_recording_platform_view(recording_platform_id):
     """
-    Renders the admin view for a specific recording platform.
-
-    Parameters:
-    recording_platform_id (uuid): The unique identifier of the recording platform.
-
-    Returns:
-    flask.Response: The rendered HTML template for the admin recording platform view.
-    """
-    with Session() as session:
-        recording_platform = session.query(RecordingPlatform).filter_by(id=recording_platform_id).first()
-        return render_template('admin/admin-recording-platform-view.html', recording_platform=recording_platform)
-
-@routes_admin.route('/admin/recording-platform/<uuid:recording_platform_id>/edit', methods=['POST'])
-def admin_recording_platform_edit(recording_platform_id):
-    """
-    A function to edit a recording platform in the admin interface.
-    
-    Parameters:
-    recording_platform_id (str): The UUID of the recording platform to edit.
-    
-    Returns:
-    None
+    A route to display a recording platform in the admin panel.
     """
     with Session() as session:
         try:
-            recording_platform = session.query(RecordingPlatform).filter_by(id=recording_platform_id).first()
-            if request.method == 'POST':
-                # Update recording platform with form data
-                recording_platform.name = request.form['name']
-                session.commit()
-                flash('Recording platform updated: {}'.format(recording_platform.name), 'success')
+            recording_platform = session.query(RecordingPlatform).filter_by(id=recording_platform_id).first()  
+            return render_template('admin/admin-recording-platform-view.html', recording_platform=recording_platform)
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
-        return redirect(url_for('admin.admin_dashboard'))
+            return redirect(url_for('admin.admin_dashboard'))
+        
+@routes_admin.route('/admin/recording-platform/<uuid:recording_platform_id>/edit', methods=['POST'])
+def admin_recording_platform_edit(recording_platform_id):
+    """
+    Updates a recording platform with the provided form data.
+    """
+    with Session() as session:
+        try:
+            # Update recording platform with form data
+            recording_platform = session.query(RecordingPlatform).filter_by(id=recording_platform_id).first()
+            recording_platform.name = request.form['name']
+            # Apply changes
+            session.commit()
+            flash('Recording platform updated: {}'.format(recording_platform.name), 'success')
+        except SQLAlchemyError as e:
+            flash(parse_alchemy_error(e), 'error')
+            session.rollback()
+    return redirect(url_for('admin.admin_dashboard'))
 
 @routes_admin.route('/admin/recording-platform/new', methods=['GET'])
 def admin_recording_platform_new():
@@ -193,14 +169,14 @@ def admin_recording_platform_insert():
             session.commit()
             flash('Recording platform created: {}'.format(new_recording_platform.name), 'success')
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
         return redirect(url_for('admin.admin_dashboard'))
 
 @routes_admin.route('/admin/recording-platform/<uuid:recording_platform_id>/delete', methods=['GET'])
 def admin_recording_platform_delete(recording_platform_id):
     """
-    A route to delete a recording platform in the admin panel. Takes the recording platform id as a parameter.
+    Delete a recording platform in the admin panel.
     """
     with Session() as session:
         try:
@@ -209,54 +185,29 @@ def admin_recording_platform_delete(recording_platform_id):
             session.commit()
             flash('Recording platform deleted: {}'.format(recording_platform.name), 'success')
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
         return redirect(url_for('admin.admin_dashboard'))
-
-
-'''
-@routes_admin.route('/species/dashboard', methods=['GET'])
-def admin_species_dashboard():
-    with Session() as session:
-        try:
-            species_data = session.query(Species).all()
-            return render_template('species/species.html', species_list=species_data)
-        except Exception as e:
-            flash(str(e), 'error')
-'''
 
 @routes_admin.route('/admin/species/<uuid:species_id>/view', methods=['GET'])
 def admin_species_view(species_id):
     """
     Edit and update species data based on the provided species ID.
-
-    Parameters:
-    - species_id: The UUID of the species to edit.
-
-    Returns:
-    - If the request method is POST and the species exists, updates the species data and redirects to '/species'.
-    - If the species does not exist, flashes an error message and redirects to '/species'.
-    - If the request method is not POST, renders the 'edit_species.html' template for editing.
-    - In case of exceptions, rolls back the session, flashes an error message, and redirects to '/species'.
     """
-    try:
-        session = Session()
-        species_data = session.query(Species).filter_by(id=species_id).first()
- 
-        return render_template('admin/admin-species-view.html', species=species_data)
-    except Exception as e:
-        session.rollback()
-        flash(str(e), 'error')
-        return redirect(url_for('admin.admin_dashboard'))
-    finally:
-        session.close()
-    
+    with Session() as session:
+        try:
+            session = Session()
+            species_data = session.query(Species).filter_by(id=species_id).first()
+            return render_template('admin/admin-species-view.html', species=species_data)
+        except Exception as e:
+            session.rollback()
+            flash(str(e), 'error')
+            return redirect(url_for('admin.admin_dashboard'))
     
 @routes_admin.route('/admin/species/<uuid:species_id>/edit', methods=['POST'])
 def admin_species_edit(species_id):
     with Session() as session:
         species_data = session.query(Species).filter_by(id=species_id).first()
-
         if species_data:
             species_name = request.form['species_name']
             genus_name = request.form['genus_name']
@@ -266,26 +217,27 @@ def admin_species_edit(species_id):
             species_data.set_common_name(common_name)
             species_data.update_call(session)
             session.commit()
-            #clean_up_root_directory(FILE_SPACE_PATH)
             flash('Species updated: {}'.format(species_name), 'success')
         else:
             flash('Species with ID {} not found'.format(species_id), 'error')
             session.rollback()
         return redirect(url_for('admin.admin_dashboard'))
 
-# Update the route handler to use SQLAlchemy for deleting a species from the table
 @routes_admin.route('/admin/species/<uuid:species_id>/delete', methods=['POST'])
 def admin_species_delete(species_id):
-    try:
-        with Session() as session:
+    """
+    A function to delete a species from the admin panel by its ID.
+    """
+    with Session() as session:
+        try:
             species = session.query(Species).filter_by(id=species_id).first()
             species_name = species.get_species_name()
             session.delete(species)
             session.commit()
             flash('Species deleted: {}'.format(species_name), 'success')
-    except SQLAlchemyError as e:
-        flash(str(e), 'error')
-        session.rollback()
+        except SQLAlchemyError as e:
+            flash(str(e), 'error')
+            session.rollback()
     return redirect(url_for('admin.admin_dashboard'))
 
     
@@ -305,7 +257,7 @@ def admin_species_insert():
             session.commit()
             flash('Species added: {}.'.format(species_name), 'success')
         except SQLAlchemyError as e:
-            flash(db.parse_alchemy_error(e), 'error')
+            flash(parse_alchemy_error(e), 'error')
             session.rollback()
     return redirect(url_for('admin.admin_dashboard'))
 
