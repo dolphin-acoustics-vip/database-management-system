ef9bd31c45625779777bb0dc11b228f4aaf5ba7e
JamieSullivan12 <sullivanj041@gmail.com>
2024-06-20 11:21:15 +0100

    Add model to store contour csv files

---
+++ calculations/rocca/contour.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2024
+#
+# This file is part of OCEAN.
+#
+# OCEAN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OCEAN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OCEAN.  If not, see <https://www.gnu.org/licenses/>.
+
+class Contour:
+    time_milliseconds: float
+    peak_frequency: float
+    duty_cycle: float
+    energy: float
+    window_RMS: float
+    
+    def __init(self):
+        pass
+    
